50c77572298837ba79f435b3145e9e5701e74153
Ethan <30705855+TheNarwhalArmy@users.noreply.github.com>
2025-05-31 19:03:02 -0400

    Update miniz.h

--- potatomelt/src/lib/miniz/miniz.h
+++ potatomelt/src/lib/miniz/miniz.h
@@ -1,13 +1,2 @@
-#ifndef MINIZ_H
-#define MINIZ_H
-
-// Define MINIZ_NO_STDIO, MINIZ_NO_TIME, etc. based on Pngle's needs or project defaults.
-// Pngle primarily needs tinfl_decompress. Default miniz.c provided by user has many disabled.
-// For Pngle, we primarily need the decompression part (tinfl).
-// The user-provided miniz.c already has many features like NO_STDIO, NO_TIME, NO_ARCHIVE_APIS, NO_ZLIB_APIS, NO_COMPRESSION defined.
-// These seem fine for Pngle's tinfl usage.
-
 #define MINIZ_HEADER_FILE_ONLY
-#include "miniz.c"
-
-#endif // MINIZ_H
+#include "./miniz.c"
